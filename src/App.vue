363fc2fd5202bad6b57bd0e7011b590fa5bdc4f6
Jonathan Reis <jonathanreis@Jonathans-MBP.lan>
2021-05-11 12:24:00 -0400

    metamask login working, ganache is set up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MetamaskLogin msg="Login using Metamask"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MetamaskLogin from './components/MetamaskLogin.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MetamaskLogin
   }
 }
 </script>
---
+++ src/components/MetamaskLogin.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+    <button v-on:click="greet" class="enableEthereumButton">Enable Ethereum</button>
+  </div>
+</template>
+
+<script>
+
+if (typeof window.ethereum !== 'undefined') {
+  console.log('MetaMask is installed!');
+}
+
+export default {
+  name: 'MetamaskLogin',
+  props: {
+    msg: String
+  },
+  methods: {
+    greet: () => {
+      window.ethereum.request({ method: 'eth_requestAccounts' });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
